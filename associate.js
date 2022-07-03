d97ab3ccd0d6ca783f04a728e3d859b8c8c1d960
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-07-03 13:05:39 -0400

    associate.js done

--- associate.js
+++ associate.js
@@ -53,7 +53,7 @@ const ticketInfo = {
 /**
  * findPrice()
  * ----------------
- * Basedon the above ticket Info object,
+ * Based on the above ticket Info object,
  * Dynamically return a string that says:
    'General Admission for a child is $20.00'
  * Your solution should work for any ticketInfo object containing
@@ -63,6 +63,8 @@ const ticketInfo = {
  * @param {Object} info 
  * @return {string}
  */
-function findPrice(tickets, info) {}
+function findPrice(tickets, info) {
+  return `${ticketTypes.general.description} for a child is $${(ticketTypes.general.priceInCents.child/100).toFixed(2)}`
+}
 
 console.log(findPrice(ticketTypes, ticketInfo));
